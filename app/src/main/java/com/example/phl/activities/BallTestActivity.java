f514104727ed8525048e62120267257463f75799
Chunhao Zou <czou40@gatech.edu>
2023-06-09 00:37:36 -0400

    update gradle config

---
+++ app/src/main/java/com/example/phl/activities/BallTestActivity.java
@@ -0,0 +1,18 @@
+package com.example.phl.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.phl.R;
+
+import com.google.mediapipe.tasks.vision.core.RunningMode;
+
+public class BallTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ball_test);
+    }
+}
