045f9ef1581ed3be51776b8baa1f79b6e69aef2f
EvaTomlinson <kkheva0215@gmail.com>
2024-04-08 19:32:18 -0400

    fixed typo

--- UnityMediaPipeHands/Assets/Scripts/HoeController.cs
+++ UnityMediaPipeHands/Assets/Scripts/HoeController.cs
@@ -13,16 +13,17 @@ public class HoeController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (dataReceiver.HasPoseData) {
+        if (dataReceiver.HasPoseData)
+        {
             float angle = dataReceiver.getLeftShoulderExtensionAngle();
-            transform.rotation = Quaternion.Euler(0, 0, -angle+90);
-            text.text = "Left Shoulder Angle: " + angle;
+            transform.rotation = Quaternion.Euler(0, 0, -angle + 90);
+            text.text = "Right Shoulder Angle: " + angle;
         }
     }
 
