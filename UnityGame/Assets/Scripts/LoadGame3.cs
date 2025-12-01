a3a4150c822c9c33135ab8899c0ca1d4d7dda6e7
Shan Patel <shanpatel371@gmail.com>
2025-11-30 17:40:13 -0800

    added game3 to home page

---
+++ UnityGame/Assets/Scripts/LoadGame3.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadGame3 : MonoBehaviour
+{
+    public void OpenGame3()
+    {
+        SceneManager.LoadScene("Game3");
+    }
+}
